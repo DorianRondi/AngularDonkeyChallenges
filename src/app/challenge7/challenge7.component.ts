ba71596bcab97f9167c7ec91c44d82dd65b9ed83
DorianRONDI <dorianrondi@gmail.com>
2023-05-28 14:32:27 +0200

    Challenge 7 Terminé

--- src/app/challenge7/challenge7.component.ts
+++ src/app/challenge7/challenge7.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
-  selector: 'app-challenge7',
+  selector: 'challenge7',
   templateUrl: './challenge7.component.html',
   styleUrls: ['./challenge7.component.scss']
 })
 export class Challenge7Component {
-
+  uzer: User = new User();
+  constructor() { }
+  onSubmit(DrivenForm: NgForm) {
+    console.log(DrivenForm);
+ }
 }
