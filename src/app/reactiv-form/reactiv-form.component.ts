8ed401063cea5bae6232dbc31927d1e04b636e1a
DorianRONDI <dorianrondi@gmail.com>
2023-05-29 16:15:36 +0200

    pas le temps avant de changé de projet pour le cours

--- src/app/reactiv-form/reactiv-form.component.ts
+++ src/app/reactiv-form/reactiv-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, MaxLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactiv-form',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactiv-form.component.scss']
 })
 export class ReactivForm {
+  reactiv : FormGroup;
 
+  constructor(){
+    this.reactiv = new FormGroup({
+      ref : new FormControl(null,[Validators.required,Validators.minLength(5),Validators.maxLength(5)]),
+      quantity : new FormControl(null,[Validators.required,Validators.min(1)]),
+      date : new FormControl('2022-01-27',Validators.required),
+      email : new FormControl(null,[Validators.required,Validators.email]),
+    })
+  }
+  onSubmit(){
+    console.log(this.reactiv);
+  }
+  get ref(){
+    return this.reactiv.get('ref') as FormControl;
+  }
+  get email(){
+    return this.reactiv.get('email') as FormControl;
+  }
+  get quantity(){
+    return this.reactiv.get('quantity') as FormControl;
+  }
+  get date(){
+    return this.reactiv.get('date') as FormControl;
+  }
 }
